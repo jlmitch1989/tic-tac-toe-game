aa89672d6b4530c51ed92506e364139a8c28c82a
jlmitch1989 <joshuamitchell@Joshuas-MacBook-Pro.local>
2016-07-27 17:09:29 -0400

    Add event handlers for all game board tiles
    Added all event handlers to tiles for clicks and
    made onMove function to respond to clicks.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -5,6 +5,8 @@ const getFormFields = require('../../../lib/get-form-fields');
 const api = require('./api');
 const ui = require('./ui');
 
+// const toePic = <img src="./assets/toe-pic.jpg">;
+// const ticTacPic = <img src="./assets/tic-tac-pic.jpg">;
 
 // SIGNIN / SIGNUP/ SIGNOUT / CHANGE PASSWORD
 const onSignUp = function (event) {
@@ -40,12 +42,25 @@ const onSignOut = function (event) {
 };
 // SIGNIN / SIGNUP/ SIGNOUT / CHANGE PASSWORD END
 
+// CLICK MOVES
+const onMove = function (event) {
+  console.log('you click me');
+};
+
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp);
   $('#sign-in').on('submit', onSignIn);
   $('#change-password').on('submit', onChangePassword);
   $('#sign-out').on('submit', onSignOut);
-
+  $('.tile-a').on('click', onMove);
+  $('.tile-b').on('click', onMove);
+  $('.tile-c').on('click', onMove);
+  $('.tile-d').on('click', onMove);
+  $('.tile-e').on('click', onMove);
+  $('.tile-f').on('click', onMove);
+  $('.tile-g').on('click', onMove);
+  $('.tile-h').on('click', onMove);
+  $('.tile-i').on('click', onMove);
 };
 
 module.exports = {
